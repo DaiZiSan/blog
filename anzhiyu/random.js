ee609d9492eeaccdf3c2ff732f9d471ee0b60954
DaiZiSan <daimaizi@192.168.2.171>
2024-09-04 21:03:49 +0800

    Site updated: 2024-09-04 21:03:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/make_blog_one_day_say.html","posts/maths.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/make_blog_one_day_say.html","posts/maths.html","posts/kxyjg.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
